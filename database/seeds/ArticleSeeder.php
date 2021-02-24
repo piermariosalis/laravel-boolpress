72b4560ff78c2d7a8ca23816e988eb7f5f40de23
piermario <piermario.salis@aol.com>
2021-02-24 01:34:30 +0100

    useing faker

--- database/seeds/ArticleSeeder.php
+++ database/seeds/ArticleSeeder.php
@@ -1,5 +1,6 @@
 <?php
 use App\Article;
+use Faker\Generator as Faker;
 use Illuminate\Database\Seeder;
 
 class ArticleSeeder extends Seeder
@@ -9,16 +10,25 @@ class ArticleSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        $articles = config('articles');
+        // $articles = config('articles');
 
-        foreach ($articles as $article) {
+        // foreach ($articles as $article) {
+        //     $newArticle = new Article();
+        //     $newArticle -> title = $article['title'];
+        //     $newArticle -> body = $article['body'];
+        //     $newArticle -> save();
+
+        // }
+
+
+        // FAKER
+        for ($i=0; $i < 20; $i++) { 
             $newArticle = new Article();
-            $newArticle -> title = $article['title'];
-            $newArticle -> body = $article['body'];
+            $newArticle -> title = $faker-> realText($maxNbChars = 10);
+            $newArticle -> body = $faker-> realText($maxNbChars = 20);
             $newArticle -> save();
-
         }
     }
 }
